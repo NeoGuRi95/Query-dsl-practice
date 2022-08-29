7ab852d74839ea9b7bd88c93170992f8c739df84
NeoGuRi95 <jea5158@gmail.com>
2022-08-29 14:40:01 +0900

    모든 회원 수 구하기

--- src/test/java/com/ll/exam/qsl/UserRepositoryTest.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTest.java
@@ -61,4 +61,12 @@ class UserRepositoryTests {
         assertThat(u2.getEmail()).isEqualTo("user2@test.com");
         assertThat(u2.getPassword()).isEqualTo("{noop}1234");
     }
+
+    @Test
+    @DisplayName("모든 회원의 수")
+    void t4() {
+        long cnt = userRepository.getQslCount();
+
+        assertThat(cnt).isEqualTo(2);
+    }
 }
